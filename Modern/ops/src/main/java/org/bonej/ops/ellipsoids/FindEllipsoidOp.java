0e2f9fb2fef1671d92208ac670a4476464c47e44
alessandrofelder <afelder@rvc.ac.uk>
2017-10-13 18:05:49 +0100

    first prototype of Ellipsoid Factor 2 ops

---
+++ Modern/ops/src/main/java/org/bonej/ops/ellipsoids/FindEllipsoidOp.java
@@ -0,0 +1,189 @@
+package org.bonej.ops.ellipsoids;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.mines.jtk.opt.Vect;
+import net.imagej.ops.Contingent;
+import net.imagej.ops.special.function.AbstractBinaryFunctionOp;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.BooleanType;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.random.UnitSphereRandomVectorGenerator;
+
+public class FindEllipsoidOp<B extends BooleanType<B>> extends AbstractBinaryFunctionOp<RandomAccessibleInterval<B>, Vector3D, Ellipsoid> implements Contingent {
+
+    private final static UnitSphereRandomVectorGenerator rvg = new UnitSphereRandomVectorGenerator(4);
+
+    @Override
+    public Ellipsoid calculate(final RandomAccessibleInterval<B> binaryImage, final Vector3D seedPoint) {
+
+        double maxSamplingRadius = 25;
+        double samplingWidth = 1.0;
+
+        int nSphere = estimateNSpiralPointsRequired(maxSamplingRadius,samplingWidth);
+        int nPlane = (int) Math.ceil(2*Math.PI*maxSamplingRadius/samplingWidth);
+
+        List<Vector3D> sphereSamplingDirections = getGeneralizedSpiralSetOnSphere(nSphere);
+
+        List<Ellipsoid> ellipsoids = sphereSamplingDirections.stream().map(dir -> getEllipsoidFromInitialAxis(seedPoint, nPlane, dir)).collect(Collectors.toList());
+
+        ellipsoids.sort((o1,o2) -> {
+                if(o1.getA()*o1.getB()*o1.getC()<o2.getA()*o2.getB()*o2.getC())
+                {
+                    return 1;
+                }
+                if(o1.getA()*o1.getB()*o1.getC()>o2.getA()*o2.getB()*o2.getC())
+                {
+                    return -1;
+                }
+                return 0;
+            });
+
+        return ellipsoids.get(0);
+    }
+
+    public Ellipsoid getEllipsoidFromInitialAxis(Vector3D seedPoint, int nPlane, Vector3D initialAxis) {
+        List<Vector3D> firstDirectionToTry = new ArrayList<>();
+        firstDirectionToTry.add(initialAxis);
+
+        Vector3D firstAxis = findClosestContact(seedPoint, firstDirectionToTry);
+
+        List<Vector3D> orthogonalSearchDirections = getOrthogonalSearchDirections(getFlooredVector3D(firstAxis), nPlane);
+
+        Vector3D secondAxis = findClosestContact(seedPoint,orthogonalSearchDirections);
+
+        List<Vector3D> thirdAxisSearchDirections = new ArrayList<>();
+        Vector3D thirdAxisSearchDirection = getFlooredVector3D(secondAxis).crossProduct(getFlooredVector3D(firstAxis)).normalize();
+        thirdAxisSearchDirections.add(thirdAxisSearchDirection);
+        thirdAxisSearchDirections.add(thirdAxisSearchDirection.scalarMultiply(-1.0));
+
+        Vector3D thirdAxis = findClosestContact(seedPoint,thirdAxisSearchDirections);
+
+        double a = Math.max(firstAxis.getNorm()-1,1.0);
+        double b = Math.max(secondAxis.getNorm()-1,1.0);
+        double c = Math.max(thirdAxis.getNorm()-1,1.0);
+
+        return new Ellipsoid(a,b,c);
+    }
+
+    public static Vector3D getFlooredVector3D(Vector3D vector) {
+        return new Vector3D(Math.floor(vector.getX()), Math.floor(vector.getY()), Math.floor(vector.getZ()));
+    }
+
+    private static List<Vector3D> getOrthogonalSearchDirections(Vector3D firstAxis, int nPlane) {
+        Rotation inPlaneRotation = new Rotation(firstAxis, 2*Math.PI/nPlane);
+
+        double [] searchDirection = rvg.nextVector();
+        Vector3D orthogonal = firstAxis.crossProduct(new Vector3D(searchDirection[0],searchDirection[1],searchDirection[2]));
+        orthogonal = orthogonal.normalize();
+
+        List<Vector3D> orthogonalSearchDirections = new ArrayList<>();
+
+        for(int k=0; k<nPlane-1; k++)
+        {
+            orthogonalSearchDirections.add(new Vector3D(1.0,orthogonal));
+            orthogonal = inPlaneRotation.applyTo(orthogonal);
+        }
+        return orthogonalSearchDirections;
+    }
+
+    private Vector3D findClosestContact(Vector3D seedPoint, List<Vector3D> samplingDirections) throws IllegalArgumentException {
+            List<Vector3D> contactPoints = new ArrayList<>();
+            samplingDirections.forEach(d -> contactPoints.add(findFirstPointInBGAlongRay(d.normalize(), seedPoint)));
+            contactPoints.sort((o1, o2) -> {
+                if (o1.subtract(seedPoint).getNorm() < o2.subtract(seedPoint).getNorm()) {
+                    return -1;
+                }
+                if (o1.subtract(seedPoint).getNorm() > o2.subtract(seedPoint).getNorm()) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            return contactPoints.get(0).subtract(seedPoint);
+    }
+
+
+    static List<Vector3D> getGeneralizedSpiralSetOnSphere(int n){
+        //follows nomenclature of Saff and Kuijlaars, 1997 describing the work of Rakhmanov et al, 1994
+        //k is shifted to the left for convenient indexing.
+        //n needs to be greater than 2.
+        List<Vector3D> spiralSet = new ArrayList<>();
+
+        List<Double> phi = new ArrayList<>();
+        phi.add(0.0);
+        for (int k=1; k<n-1;k++)
+        {
+            double h = -1.0+2.0*((double)k)/(n-1);
+            phi.add(getPhiByRecursion(n, phi.get(k - 1), h));
+        }
+        phi.add(0.0);
+
+        for(int k=0; k<n; k++)
+        {
+            double h = -1.0+2.0*((double)k)/(n-1);
+            double theta = Math.acos(h);
+             spiralSet.add(new Vector3D(Math.sin(theta)*Math.cos(phi.get(k)), Math.sin(theta)*Math.sin(phi.get(k)), Math.cos(theta)));
+
+        }
+
+        return spiralSet;
+    }
+
+
+    private static double getPhiByRecursion(double n, double phiKMinus1, double hk) {
+        double phiK = phiKMinus1+3.6/Math.sqrt(n)*1.0/Math.sqrt(1-hk*hk);
+        //modulo 2pi calculation works for positive numbers only, which is not a problem in this case.
+        return phiK - Math.floor(phiK/(2*Math.PI))*2*Math.PI;
+    }
+
+    private static int estimateNSpiralPointsRequired(double searchRadius, double pixelWidth)
+    {
+        return (int) Math.ceil(Math.pow(searchRadius*3.809/pixelWidth,2));
+    }
+
+    Vector3D findFirstPointInBGAlongRay(final Vector3D rayIncrement, final Vector3D start)
+    {
+        RandomAccess<B> randomAccess = in1().randomAccess();
+
+        Vector3D currentRealPosition = start;
+        long[] currentPixelPosition = vectorToPixelGrid(start);
+        randomAccess.setPosition(currentPixelPosition);
+
+        while(randomAccess.get().get())
+        {
+            currentRealPosition = currentRealPosition.add(rayIncrement);
+            currentPixelPosition = vectorToPixelGrid(currentRealPosition);
+            if(!isInBounds(currentPixelPosition)) break;
+            randomAccess.setPosition(currentPixelPosition);
+        }
+        return currentRealPosition;
+    }
+
+    private boolean isInBounds(long[] currentPixelPosition) {
+        long width = in1().max(0);
+        long height = in1().max(1);
+        long depth = in1().max(2);
+        return currentPixelPosition[0]>0 && currentPixelPosition[0]<width && currentPixelPosition[1]>0 && currentPixelPosition[1]<height && currentPixelPosition[2]>0 && currentPixelPosition[2]<depth;
+    }
+
+    private long[] vectorToPixelGrid(Vector3D currentPosition) {
+        double[] dArray = currentPosition.toArray();
+        long[] lArray = new long[3];
+        lArray[0] = (long) dArray[0];
+        lArray[1] = (long) dArray[1];
+        lArray[2] = (long) dArray[2];
+        return lArray;
+    }
+
+    @Override
+    public boolean conforms() {
+        return in1().numDimensions() == 3;
+    }
+}
---
+++ Modern/ops/src/test/java/org/bonej/ops/ellipsoids/FindEllipsoidOpTest.java
@@ -0,0 +1,165 @@
+package org.bonej.ops.ellipsoids;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import net.imagej.ImgPlus;
+import net.imglib2.Cursor;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.junit.Test;
+
+public class FindEllipsoidOpTest {
+
+    @Test
+    public void testMaximalEllipsoidInConcaveSpace()
+    {
+        long[] imageDimensions = {101,101,101};
+        Vector3D imageCentre = new Vector3D(Math.floor(imageDimensions[0]/2.0),Math.floor(imageDimensions[1]/2.0),Math.floor(imageDimensions[2]/2.0));
+        long[] semiAxes1 = {2,3,1};
+        long[] semiAxes2 = {6,1,1};
+
+        final Img<BitType> img = ArrayImgs.bits(imageDimensions[0], imageDimensions[1], imageDimensions[2]);
+        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "Cylinder test image");
+        Cursor<BitType> cursor = imgPlus.localizingCursor();
+
+        while(cursor.hasNext())
+        {
+            cursor.fwd();
+            long [] coordinates = new long[3];
+            cursor.localize(coordinates);
+            if(coordinates[2]==0 || coordinates[2]==imageDimensions[2]-1) continue;
+            double x = imageCentre.getX()-coordinates[0];
+            double y = imageCentre.getY()-coordinates[1];
+            double z = imageCentre.getZ()-coordinates[2];
+
+            boolean condition1 = ((x*x)/(semiAxes1[0]*semiAxes1[0])+(y*y)/(semiAxes1[1]*semiAxes1[1])+z*z/(semiAxes1[2]*semiAxes1[2])<=1.0);
+            boolean condition2 = ((x*x)/(semiAxes2[0]*semiAxes2[0])+(y*y)/(semiAxes2[1]*semiAxes2[1])+z*z/(semiAxes2[2]*semiAxes2[2])<=1.0);
+            if(condition1 || condition2)
+                cursor.get().setOne();
+        }
+
+        FindEllipsoidOp<BitType> findEllipsoidOp = new FindEllipsoidOp<>();
+
+        findEllipsoidOp.setInput1(imgPlus);
+        findEllipsoidOp.setInput2(imageCentre);
+
+        Ellipsoid maxEllipsoid = findEllipsoidOp.calculate();
+
+        assertEquals(6.0, maxEllipsoid.getA()*maxEllipsoid.getB()*maxEllipsoid.getC(), 1+1e-12);
+
+    }
+
+    @Test
+    public void testContactPointsInCylinder()
+    {
+        long[] imageDimensions = {101,101,101};
+        Vector3D imageCentre = new Vector3D(Math.floor(imageDimensions[0]/2.0),Math.floor(imageDimensions[1]/2.0),Math.floor(imageDimensions[2]/2.0));
+        long cylinderRadius = 23;
+
+        final Img<BitType> img = ArrayImgs.bits(imageDimensions[0], imageDimensions[1], imageDimensions[2]);
+        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "Cylinder test image");
+        Cursor<BitType> cursor = imgPlus.localizingCursor();
+
+        while(cursor.hasNext())
+        {
+            cursor.fwd();
+            long [] coordinates = new long[3];
+            cursor.localize(coordinates);
+            if(coordinates[2]==0 || coordinates[2]==imageDimensions[2]-1) continue;
+            double x = imageCentre.getX()-coordinates[0];
+            double y = imageCentre.getY()-coordinates[1];
+            double distanceFromCentreLine = x*x+y*y;
+            if( distanceFromCentreLine <= cylinderRadius*cylinderRadius)
+                cursor.get().setOne();
+        }
+
+        FindEllipsoidOp<BitType> findEllipsoidOp = new FindEllipsoidOp<>();
+
+        findEllipsoidOp.setInput1(imgPlus);
+        findEllipsoidOp.setInput2(imageCentre);
+
+        Ellipsoid maxEllipsoid = findEllipsoidOp.calculate();
+
+        //expected value is pixel next to axis-aligned circle.
+        assertEquals(cylinderRadius, maxEllipsoid.getA(), 1+1e-12);
+        assertEquals(cylinderRadius, maxEllipsoid.getB(), 1+1e-12);
+        assertEquals(Math.floor(imageDimensions[2]/2.0), maxEllipsoid.getC(), 5+1e-12);
+
+    }
+
+    @Test
+    public void testRayInEmptyImage(){
+        final Img<BitType> img = ArrayImgs.bits(10, 10, 10);
+        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "Empty test image");
+
+        FindEllipsoidOp<BitType> FindEllipsoidOp = new FindEllipsoidOp<>();
+        FindEllipsoidOp.setInput1(imgPlus);
+        Vector3D lastFGVoxelAlongRay = FindEllipsoidOp.findFirstPointInBGAlongRay(new Vector3D(1, 0, 0), new Vector3D(5, 5, 5));
+
+        assertEquals(5,lastFGVoxelAlongRay.getX(),1.0e-12);
+        assertEquals(5,lastFGVoxelAlongRay.getY(),1.0e-12);
+        assertEquals(5,lastFGVoxelAlongRay.getZ(),1.0e-12);
+    }
+
+    @Test
+    public void testRayInForegroundOnlyImage(){
+        final Img<BitType> img = ArrayImgs.bits(10, 10, 10);
+        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "Foreground only test image");
+        Cursor<BitType> cursor = imgPlus.cursor();
+
+        cursor.forEachRemaining(BitType::setOne);
+
+        FindEllipsoidOp<BitType> FindEllipsoidOp = new FindEllipsoidOp<>();
+        FindEllipsoidOp.setInput1(imgPlus);
+        Vector3D lastFGVoxelAlongRay = FindEllipsoidOp.findFirstPointInBGAlongRay(new Vector3D(1, 0, 0), new Vector3D(5, 5, 5));
+
+        assertEquals(9,lastFGVoxelAlongRay.getX(),1.0e-12);
+        assertEquals(5,lastFGVoxelAlongRay.getY(),1.0e-12);
+        assertEquals(5,lastFGVoxelAlongRay.getZ(),1.0e-12);
+    }
+
+    @Test
+    public void testRayInSphere(){
+        final Img<BitType> img = ArrayImgs.bits(101, 101, 101);
+        final ImgPlus<BitType> imgPlus = new ImgPlus<>(img, "Sphere test image");
+        IntervalView<BitType> imgCentre = Views.interval(imgPlus, new long[]{25, 25, 25}, new long[]{75, 75, 75});
+        Cursor<BitType> cursor = imgCentre.localizingCursor();
+
+        while(cursor.hasNext())
+        {
+            cursor.fwd();
+            long [] coordinates = new long[3];
+            cursor.localize(coordinates);
+            if((50-coordinates[0])*(50-coordinates[0])+(50-coordinates[1])*(50-coordinates[1])+(50-coordinates[2])*(50-coordinates[2]) <= 625)
+                cursor.get().setOne();
+        }
+
+        FindEllipsoidOp<BitType> FindEllipsoidOp = new FindEllipsoidOp<>();
+        FindEllipsoidOp.setInput1(imgPlus);
+        Vector3D alongRay1 = FindEllipsoidOp.findFirstPointInBGAlongRay(new Vector3D(-4, -3, -5).normalize(), new Vector3D(50, 50, 50));
+        Vector3D alongRay2 = FindEllipsoidOp.findFirstPointInBGAlongRay(new Vector3D(1, 1, 1).normalize(), new Vector3D(50, 50, 50));
+
+        alongRay1 = FindEllipsoidOp.getFlooredVector3D(alongRay1);
+        alongRay2 = FindEllipsoidOp.getFlooredVector3D(alongRay2);
+
+        Vector3D radialVector1 = alongRay1.subtract(new Vector3D(50,50,50));
+        Vector3D radialVector2 = alongRay2.subtract(new Vector3D(50,50,50));
+        assertEquals(26,radialVector1.getNorm(),0.2);
+        assertEquals(26,radialVector2.getNorm(),0.2);
+    }
+
+
+    //main method for manual visual testing
+    public static void main(String[] args)
+    {
+        List<Vector3D> spiralVectors = FindEllipsoidOp.getGeneralizedSpiralSetOnSphere(700);
+        spiralVectors.forEach(v -> System.out.println(v.getX()+","+v.getY()+","+v.getZ()));
+    }
+}
